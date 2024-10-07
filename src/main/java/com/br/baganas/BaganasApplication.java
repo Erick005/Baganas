0797d683512468cf6985bf986eefc04e9d64381c
Erick005 <erickportugal874@gmail.com>
2024-10-07 11:40:20 -0300

    Initial commit

---
+++ src/main/java/com/br/baganas/BaganasApplication.java
@@ -0,0 +1,13 @@
+package com.br.baganas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaganasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaganasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/baganas/BaganasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.baganas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaganasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
